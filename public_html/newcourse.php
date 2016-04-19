0c8ab0ceb58096d8904593584847dab1268a0f31
Digambaran <arjunsreekumar.1@gmail.com>
2016-04-20 03:44:35 +0530

    3:44 am

---
+++ public_html/newcourse.php
@@ -0,0 +1,19 @@
+<div>
+	<form role="form">
+		<div class="form-horizontal">
+			<label class="form-label-control" for="departmentname">Dept name:</label>
+			<select class="form-control" id="departmentname">
+				<option>ME</option>
+				<option>IT</option>
+				<option>EEE</option>
+				<option>CE</option>
+				<option>EC</option>
+				<option>CS</option>
+			</select> 
+		</div>
+	</form>
+	<form role="form">
+		<div class="form-group">
+		</div>
+	</form>
+</div>
