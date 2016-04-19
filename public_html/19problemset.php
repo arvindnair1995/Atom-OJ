c1e76d1b302db5d265616c87fec793ca67c0a229
Digambaran <arjunsreekumar.1@gmail.com>
2016-04-19 23:22:31 +0530

    some more files added.one day before submission

---
+++ public_html/19problemset.php
@@ -0,0 +1,46 @@
+<table class="table table-hover table-condensed">
+	<thead>
+		<tr>
+			<th>Slno</th>
+			<th>Problem_ID</th>
+			<th>Problem_name</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td><button class="btn btn-md btn-warning">EDIT</button></td>
+			<td><button class="btn btn-md btn-danger">REMOVE</button></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td><button class="btn btn-md btn-warning">EDIT</button></td>
+			<td><button class="btn btn-md btn-danger">REMOVE</button></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td><button class="btn btn-md btn-warning">EDIT</button></td>
+			<td><button class="btn btn-md btn-danger">REMOVE</button></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td><button class="btn btn-md btn-warning">EDIT</button></td>
+			<td><button class="btn btn-md btn-danger">REMOVE</button></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td><button class="btn btn-md btn-warning">EDIT</button></td>
+			<td><button class="btn btn-md btn-danger">REMOVE</button></td>
+		</tr>
+	</tbody>
+</table>
