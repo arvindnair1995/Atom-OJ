39b6e30b1f205a43681d65616a125bc14769e63d
Ajith Vijayan <jthvjn@gmail.com>
2016-04-21 10:31:22 +0530

    21/04

--- public_html/php/student_dashboard_load_content.php
+++ public_html/php/student_dashboard_load_content.php
@@ -1,14 +1,17 @@
 <?php
 session_start();
+include_once 'connect.php';
+
 
 if($_GET['value'] == 'active') {
+    $sql = "";
 
 }
 else if($_GET['value'] == 'archived') {
 
 }
 else if($_GET['value'] == 'new') {
-    
+
 }
 
 ?>
