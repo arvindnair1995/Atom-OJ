72f362ecfbab81e39ee60bcbf31405ad21883747
arvindnair1995 <rvghostrider@gmail.com>
2016-04-24 14:57:03 +0530

    Initial commit

---
+++ public_html/FAQ.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/faq.css" media="screen" title="no title" charset="utf-8">
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="jss/script.js" ></script>
+   </head>
+
+<body>
+    <!--********************* H E A D E R *******************-->
+    <header></header>
+    <!--***************** N A V I G A T I O N ***************-->
+    <div id="navigation"><?php
+        include_once "php/master_template_nav.php";
+        ?>
+
+    </div>
+
+    <div id="content">
+
+            <p id="head">
+              Frequently Asked Questions
+            </p>
+
+                <p>
+                    What are the features of this system?
+                <br>
+                   This is a Programming Lab Management System which automates most of the trivial tasks in a programming lab course.
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    How to obtain the passcode?
+                <br>
+                   Ask the staff in-charge to provide you with one.
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    What are Active and Archived courses?
+                <br>
+                    Active courses are those courses which are included in the current semster and archived are those which were included in the previous semesters.
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    What are the different sections of a problem page?
+                <br>
+                   The problem page is divided into 4 sections- Problem statement, Constraints, Sample Input and Sample Output.
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    How do we know if our algorithm is correct or not?
+                <br>
+                    The agorithm will be checked by staff-in-charge and the user will be notified of the result.
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    How do I submit a solution?
+                <br>
+                    You can either choose a file or write your code in the algo editor and submit it.
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    How do I know if my program is correct or not?
+                <br>
+                    The submitted program will be checked against hidden test cases and the user will recieve a status depending on the result. You can view your status in the Status page. Status Codes are-
+                  <br>
+                  <ul>
+                    <li id="p1"> Accepted</li>
+                    <li id="p2"> Algo correct, Program Wrong</li>
+                    <li id="p3"> Algo Wrong</li>
+                  </ul>
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    What do I do if I forgot the password?
+                <br>
+                    Conatct the Staff-in-charge.
+                </p>
+
+                <br>
+                <br>
+
+                <p>
+                    Who are the creators?
+                <br>
+                    Ajith Vijayan, Arjun S Kumar, Haripriya S, Arvind K Nair ( 2012 Computer Science batch ).
+                </p>
+
+
+    </div>
+    <!--********************* S I D E  B A R ****************-->
+    <aside></aside>
+    <!--********************* F O O T E R *******************-->
+    <div class="container-fluid" style="padding: 0px;"><footer><hr class="s"></footer></div>
+</div>
+</body>
+</html>
